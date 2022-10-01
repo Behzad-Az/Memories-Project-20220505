d61bc5f3bc56a7166f0a63d2dd2711a9fcc61652
Behzad-Az <behy1368@gmail.com>
2022-10-01 09:16:59 -0700

    refactoring

--- server/index.js
+++ server/index.js
@@ -7,22 +7,16 @@ import env from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 env.config();
-const { MONGODB_USERNAME, MONGODB_PASSWORD } = process.env;
+const { CONNECTION_URL, PORT } = process.env;
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
 app.use('/posts', postRoutes);
-
-const CONNECTION_URL = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.xejzknd.mongodb.net/?retryWrites=true&w=majority`
-
-const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
   .catch(error => console.log(error.message));
-
-// mongoose.set('useFindAndModify', false);
